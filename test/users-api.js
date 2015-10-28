ca18ab362668e53df484e4acb10034908d89018e
Joao Diogo Falcao <john.diogo.m.falcao@gmail.com>
2015-10-28 12:23:35 +0000

    Test users API

---
+++ test/users-api.js
@@ -0,0 +1,17 @@
+var expect = require('expect.js');
+
+suite('Users API', function(){
+
+  test('Creating user');
+
+  test('Getting user');
+
+  test('User format');
+
+  test('User Data Validity');
+
+  test('Deleting user');
+
+  test('Updating user');
+
+});
