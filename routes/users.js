e6149885eebd072892b9529ed408170f3f005d5c
Joao Diogo Falcao <john.diogo.m.falcao@gmail.com>
2015-10-28 14:11:12 +0000

    Simpe Users behaviour - Create, Delete, Get

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* Mock User */
-var Users = [{
-  name: 'John',
-  surname: 'Doe',
-  password: 'mypassword',
-  age: 35,
-  address: 'Moffet Field, Nasa Research Park'
-}];
+var Users = [];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -20,10 +14,31 @@ router.get('/:name', function(req, res, next) {
   for(var i=0;i<Users.length;i++){
       if(Users[i].name === req.params.name){
         res.status(200).json(Users[i]);
-        return next();
+        return;
       }
   }
   res.status(404).send('User not found!');
+});
+
+router.post('/', function(req, res, next){
+  Users.push(req.body);
+  res.status(201).end();
+});
+
+router.delete('/:name', function(req, res, next){
+  for(var i=0; i < Users.length; i++) {
+    var obj = Users[i];
+
+    if(obj.name === req.params.name) {
+        Users.splice(i, 1);
+    }
+  }
+  res.status(200).end();
+});
+
+router.delete('/', function(req, res, next){
+  Users = [];
+  res.status(200).end();
 });
 
 module.exports = router;
