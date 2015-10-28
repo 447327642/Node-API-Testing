9f0cc062a6337c4e02838172473f5630468bd3fe
Joao Diogo Falcao <john.diogo.m.falcao@gmail.com>
2015-10-28 14:11:31 +0000

    Testing Users API

--- test/users-api.js
+++ test/users-api.js
@@ -1,17 +1,159 @@
 var expect = require('expect.js');
+var agent = require('superagent');
+
+var PORT = process.env.PORT | 3000;
+var HOST = 'http://localhost:'+PORT;
+
+// Initiate Server
+var debug = require('debug')('Node-API-Testing');
+var app = require('../app');
+
+app.set('port', PORT);
+app.set('testing', true);
+
+var server = app.listen(app.get('port'), function() {
+  debug('Express server listening on port ' + server.address().port);
+});
+//////////////////////////////////
+
+var user = {
+  name: 'John',
+  surname: 'Doe',
+  age: 35
+};
 
 suite('Users API', function(){
 
-  test('Creating user');
+  test('Creating user', function(done){
+    agent.post(HOST+'/users')
+    .send(user)
+    .end(function(err, res){
+      expect(err).to.not.be.ok();
+      expect(res).to.have.property('statusCode');
+      expect(res).to.have.property('body');
+      expect(res.statusCode).to.equal(201);
+      done();
+    });
+  });
 
-  test('Getting user');
+  test('Deleting all Users', function(done){
+    agent.del(HOST+'/users')
+    .end(function(err, res){
+      expect(err).to.not.be.ok();
+      expect(res).to.have.property('statusCode');
+      expect(res).to.have.property('body');
+      expect(res.statusCode).to.equal(200);
 
-  test('User format');
+      agent.get(HOST+'/users')
+      .end(function(err, res){
+        expect(err).to.not.be.ok();
+        expect(res).to.have.property('statusCode');
+        expect(res).to.have.property('body');
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.an('array');
+        expect(res.body.length).to.equal(0);
+        done();
+      });
+    });
+  });
 
-  test('User Data Validity');
+  test('Getting all users when empty', function(done){
+    agent.get(HOST+'/users')
+    .end(function(err, res){
+      expect(err).to.not.be.ok();
+      expect(res).to.have.property('statusCode');
+      expect(res).to.have.property('body');
+      expect(res.statusCode).to.equal(200);
+      expect(res.body).to.be.an('array');
+      expect(res.body.length).to.equal(0);
+      done();
+    });
+  });
 
-  test('Deleting user');
+  suite('After Creating User', function(){
 
-  test('Updating user');
+    setup('Create User', function(done){
+      agent.post(HOST+'/users')
+      .send(user)
+      .end(function(){
+        done();
+      });
+    });
+
+    teardown('Delete all Users', function(done){
+      agent.del(HOST+'/users')
+      .end(function(){
+        done();
+      });
+    });
+
+    test('Getting valid user', function(done){
+        agent.get(HOST+'/users/'+user.name)
+        .end(function(err, res){
+          expect(err).to.not.be.ok();
+          expect(res).to.have.property('statusCode');
+          expect(res).to.have.property('body');
+          expect(res.statusCode).to.equal(200);
+          expect(res.body).to.be.an('object');
+          done();
+        });
+    });
+
+    test('Getting invalid user', function(done){
+      agent.get(HOST+'/users/dummy')
+      .end(function(err, res){
+        expect(err).to.be.ok();
+        expect(res).to.have.property('statusCode');
+        expect(res).to.have.property('body');
+        expect(res.statusCode).to.equal(404);
+        expect(res.body).to.be.an('object');
+        done();
+      });
+    });
+
+    test('Getting user format', function(done){
+        agent.get(HOST+'/users/'+user.name)
+        .end(function(err, res){
+          expect(res.body).to.have.property('name');
+          expect(res.body).to.have.property('surname');
+          expect(res.body).to.have.property('age');
+          expect(res.body.age).to.be.a('number');
+          done();
+        });
+    });
+
+
+    test('User Data Validity', function(done){
+        agent.get(HOST+'/users/'+user.name)
+        .end(function(err, res){
+          expect(res.body).to.eql(user);
+          done();
+        });
+    });
+
+    test('Deleting user', function(done){
+        agent.del(HOST+'/users/'+user.name)
+        .end(function(err, res){
+          expect(err).to.not.be.ok();
+          expect(res).to.have.property('statusCode');
+          expect(res).to.have.property('body');
+          expect(res.statusCode).to.equal(200);
+          // Get all Users
+          agent.get(HOST+'/users')
+          .end(function(err, res){
+            expect(err).to.not.be.ok();
+            expect(res).to.have.property('statusCode');
+            expect(res).to.have.property('body');
+            expect(res.statusCode).to.equal(200);
+            expect(res.body).to.be.an('array');
+            expect(res.body.length).to.equal(0);
+            done();
+          });
+        });
+    });
+  });
+
+
+
 
 });
