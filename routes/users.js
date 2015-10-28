ba6f7f0cb1d1879077ab1537594732982625bcb7
Joao Diogo Falcao <john.diogo.m.falcao@gmail.com>
2015-10-28 12:18:23 +0000

    mock users behaviour

--- routes/users.js
+++ routes/users.js
@@ -2,17 +2,29 @@ var express = require('express');
 var router = express.Router();
 
 /* Mock User */
-var User = {
+var Users = [{
   name: 'John',
   surname: 'Doe',
   password: 'jnafjbnlsjdfblsjdkbflskjdfb',
   age: 35,
   address: 'Moffet Field, Nasa Research Park'
-};
+}];
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/', function(req, res, next) {
+  res.status(200).json(Users);
+});
+
+/* GET user . */
+router.get('/:name', function(req, res, next) {
+  console.log(params);
+  for(var i=0;i<Users.length;i++){
+      if(Users[i].name === req.params.name){
+        res.status(200).json(Users[i]);
+        return next();
+      }
+  }
+  res.status(404).end();
 });
 
 module.exports = router;
